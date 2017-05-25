c295e2baadd33d87f4f39c7feb809007c275c7c4
Shirsh Zibbu <zibbu.shirsh+github@gmail.com>
2017-05-26 02:19:04 +0530

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AppWithRouter from './AppWithRouter.jsx';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<AppWithRouter />, document.getElementById('root'));
-registerServiceWorker();
