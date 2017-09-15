5be902df375beb8b65bec03498ead5cef0885211
Shirsh Zibbu <zibbu.shirsh+github@gmail.com>
2017-09-15 15:57:44 +0530

    add Modal.jsx

---
+++ src/Modal.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Modal extends Component {
+  render() {
+    return (
+      <div>
+        <div />
+      </div>
+    );
+  }
+}
+
+export default Modal;
