5ad1c2c1fa10b677a38ae2ff0b463bef0282b878
Shirsh Zibbu <zibbu.shirsh+github@gmail.com>
2017-09-15 16:31:34 +0530

    add twitter modals

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,10 +1,30 @@
 import React, { Component } from "react";
 
 class Modal extends Component {
+  onClose = () => {
+    this.props.history.goBack();
+  };
+
   render() {
     return (
-      <div>
-        <div />
+      <div className="modal" style={{ display: "block" }}>
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">Modal title</h5>
+              <button
+                className="close"
+                onClick={this.props.onClose || this.onClose}
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+
+            <div className="modal-body">
+              <p>Modal body</p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/Nav.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+class Nav extends React.Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <Link className="navbar-brand" to="/">
+          Home
+        </Link>
+
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link
+              className="nav-link"
+              to={{ pathname: "/twitter", state: { modal: true } }}
+            >
+              Twitter
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Nav;
