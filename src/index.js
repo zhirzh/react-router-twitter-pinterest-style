74c91fa04026befbbcd3a7ce57992e394a1721f2
Shirsh Zibbu <zibbu.shirsh+github@gmail.com>
2017-09-15 20:35:09 +0530

    replace browserrouter with hashrouter for github demo

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <Route component={App} />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root")
 );
 registerServiceWorker();
